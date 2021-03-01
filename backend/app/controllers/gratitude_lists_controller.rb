c911083fff0d8b1992f3336333b43a9415bf51af
tgarayua <tgarayua@gmail.com>
2021-02-28 21:04:13 -0500

    Finished Journal Post Controller

--- backend/app/controllers/gratitude_lists_controller.rb
+++ backend/app/controllers/gratitude_lists_controller.rb
@@ -1,6 +1,13 @@
 class GratitudeListsController < ApplicationController
 
     def index
-        
+        @gratitude_lists = GratitudeList.all
     end
+
+    def create
+        @user = User.create!(user_params)
+    
+        redirect_to user_path(@user)
+      end
+
 end
--- backend/app/controllers/journal_posts_controller.rb
+++ backend/app/controllers/journal_posts_controller.rb
@@ -1,2 +1,56 @@
 class JournalPostsController < ApplicationController
+
+    def index 
+        journal_posts = JournalPost.all
+        render json: journal_posts
+    end 
+
+    def show
+        journal_post = JournalPost.find_by(id: params[:id])
+        if journal_post
+            render json: journal_post
+        else
+            render json: { error: "Journal post not found" }, status :not_found
+        end
+    end
+
+    def create
+        current_user = User.first
+
+        journal_post = current_user.journal_posts.create(journal_post_params)
+        if journal_post.valid?
+            render json: journal_post
+        else
+            render json: { error: journal_post.errors.full_messages }, status: :unprocessable_entity
+        end
+    end
+
+    def edit
+        # journal_post = JournalPost.find(params[:id])
+
+        render :edit
+    end
+
+    def update
+        # journal_post = JournalPost.find(params[:id])
+        journal_post.update(journal_post_params)
+
+        redirect_to journal_post_path(journal_post)
+    end
+
+    def destroy
+        # journal_post = JournalPost.find(params[:id])
+        journal_post.destroy
+    end
+
+    private
+
+    def journal_post_params
+        params.permit(:title, :content)
+    end
+
+    def find_journal_post
+        journal_post = JournalPost.find(params[:id])
+    end
+
 end
